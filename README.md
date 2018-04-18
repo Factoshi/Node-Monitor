4eca3a0ea89586069c07b6c9a29710117b2d5340
AlexCarrithers <38507729+AlexCarrithers@users.noreply.github.com>
2018-04-18 20:54:57 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,66 @@
+# Factom Node Monitor and Alert
+
+This scirpt monitors the state of your remote Factom node. In the event that factomd hangs or the blockchain stalls, the script uses Twilio to make a telephone call directly to a number of your choice. Note, you must have a Twilio account to use this script.
+
+### Installing on Ubuntu
+
+Install node.js and npm if you do not already have them:
+
+```
+Sudo apt install npm
+```
+
+Install the Factom NPM library
+
+```
+npm install --save factom
+```
+
+Install Twilio
+
+```
+npm install --save twilio
+```
+
+Clone Factom Node Monitor, then open monitor.js in a text editor. Get your Twilio login details from the Twilio website and put them in the following place:
+
+```
+const accountSid = 'XXXXX';
+const authToken = 'XXXXX';
+```
+Next fill in your telephone information. Use the number provided by Twilio in the 'from' field, and your target phone number in the 'to' field:
+
+```
+url: 'http://demo.twilio.com/docs/voice.xml',
+to: '+XXXX',
+from: '+XXXX',
+```
+
+Finally, fill in your node information. Leave the host and port fields blank demoing the script on a local host with factomd on port 8088. Otherwise, fill in the IP address of your remote host and the port to call factomd on (default 8088):
+
+```
+const cli = new FactomCli({
+    host: 'HOST_IP_HERE',
+    port: 8088
+```
+
+## Running
+
+Now, make sure your factomd instance is running. Open your command line prompt and navigate to the enclosing folder. Then to run, type:
+
+```
+node monitor.js
+```
+
+## Built With
+
+* [Factom Node library designed by @PaulBernier](http://www.dropwizard.io/1.0.2/docs/)
+* [Twilio](https://www.twilio.com/) - Telephone calling utility
+
+## Authors
+
+* **Alex Carrithers**
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ node-monitor.js
@@ -0,0 +1,164 @@
+// twilio library and account login details
+const accountSid = 'XXXX';
+const authToken = 'XXXX';
+const Twilio = require('twilio');
+const client = new Twilio(accountSid, authToken);
+
+// factom library and host details, empty object defaults to local host
+const { FactomCli } = require('factom');
+const cli = new FactomCli({
+    host: 'XXXX',
+    port: 8088
+});
+
+// the number of retries made since node failure
+// length of time (ms) between each attempt to call the node
+// length of time (ms) after which blockchain is assumed to have stalled
+let retryCount = 0,
+    interval = 15000,
+    stallTime = 120000;
+
+// object to store blockchain state and time first seen by this script
+let lastBlock = {
+    blockHeight: 1,
+    timeStamp: 0,
+};
+
+/* this function takes the details given from twilio and will make a telephone call
+to the given operator */
+
+function callMe(fault) {
+    client.api.calls
+        .create({
+            url: 'http://demo.twilio.com/docs/voice.xml',
+            to: '+XXXX',
+            from: '+XXXX',
+        });
+    console.log("Alerting contact via voice call");
+    nodeOffline(fault);
+}
+
+// function to test whether a node is live
+function testNodeStatus() {
+// setInterval calls the 'heights' method on the factomd remote host. time taken from interval var
+    let testInterval = setInterval( async() => {
+        try {
+            let db = await cli.factomdApi('heights');
+/* check to see if the most recent factomd api call found a new directory block height
+if a new directly block height is found, update the lastBlock object */
+            if (db.directoryblockheight > lastBlock.blockHeight) {
+                lastBlock.blockHeight = db.directoryblockheight;
+                lastBlock.timeStamp = Date.now();
+                console.log("New block height found:", lastBlock.blockHeight);
+            }
+/* if no new directory block height is found, check the length of time since the last block.
+where more than var stallTime has elapsed, assume a stall and calls the callMe function */
+            else if (db.directoryblockheight == lastBlock.blockHeight) {
+                let elapsedTime = Date.now() - lastBlock.timeStamp,
+                    humanTime = millisToMinutesAndSeconds(elapsedTime);
+                console.log("Block", lastBlock.blockHeight, "first seen", humanTime, "minutes ago");
+                if (elapsedTime > stallTime) {
+                    console.log("Blockchain has stalled...");
+                    clearInterval(testInterval);
+                    callMe("stall");
+                }
+            }
+// if the call to the factomd api fails, callMe is triggered
+        }
+        catch(e) {
+            if (lastBlock.timeStamp == 0) {
+                console.log("Start the node before running this script");
+            }
+            else {
+                clearInterval(testInterval);
+                retry();
+            }
+        }
+    }, interval);
+}
+
+/* function called on connection failure. retries the factom api 3 times.
+if those attempts fail, callMe() is called */
+function retry() {
+    setTimeout( async() => {
+// if a try succeeds, retryCount resets to 0 and testInterval continues
+        try {
+            let db = await cli.factomdApi('heights');
+            console.log("Connection successful.");
+            retryCount = 0;
+            testNodeStatus();
+        }
+        catch(e) {
+// the first catch conditional tests retryCount, where less than 3 retries, retry
+            if (retryCount < 3) {
+                console.log("Connection failed: trying again");
+                retryCount++
+                retry();
+            }
+// where all retries have failed, callMe and clear all intervals
+            else if (retryCount == 3) {
+                retryCount = 0;
+                callMe("hang");
+            }
+        }
+    }, interval);
+}
+
+// function checks the status of an offline node. Fault type is passed in from callMe() function
+function nodeOffline(fault) {
+    let faulted = Date.now();
+// gets human readable time since stall given in minutes and seconds
+    function getTimeSinceFault() {
+        let timeOffline = Date.now() - faulted,
+            timeOfflineHuman = millisToMinutesAndSeconds(timeOffline);
+        return timeOfflineHuman;
+      }
+/* if the node has hung, log the status of the node every 60 seconds. When the node
+comes back online, break the loop and jump back to the testNodeStatus() function */
+    if (fault == "hang") {
+        let hangingInterval = setInterval( async() => {
+            try {
+                let db = await cli.factomdApi('heights');
+                console.log("Node online. Total time offline:", getTimeSinceFault(), "minutes");
+                clearInterval(hangingInterval);
+                db.directoryblockheight > lastBlock.blockHeight ? testNodeStatus() : nodeOffline("stall"),
+                console.log("Monitor.js will report node as stalled until blockchain progresses following boot");
+            }
+            catch(e) {
+                console.log("Node offline for", getTimeSinceFault(), "minutes");
+            }
+        }, 60000);
+    }
+/* if the node has stalled, wait for the block height to begin increasing again. When
+that happens, break the loop and jump back to the testNodeStatus() function */
+    else if (fault == "stall") {
+        let stallInterval = setInterval( async() => {
+            try {
+                let db = await cli.factomdApi('heights');
+                if (db.directoryblockheight > lastBlock.blockHeight) {
+                    console.log("Blockchain live again. Total time stalled:", getTimeSinceFault(), "minutes");
+                    clearInterval(stallInterval);
+                    testNodeStatus();
+                }
+                else {
+                  console.log("Node stalled for", getTimeSinceFault(), "minutes");
+                }
+            }
+// if the stall conditional fails to contact the node, return the hang conditional
+            catch(e) {
+                clearInterval(stallInterval);
+                console.log("Failed to contact stalled node...");
+                nodeOffline("hang");
+            }
+        }, 60000);
+    }
+}
+
+// convert unix time to minutes and seconds
+function millisToMinutesAndSeconds(millis) {
+    let minutes = Math.floor(millis / 60000),
+        seconds = ((millis % 60000) / 1000).toFixed(0);
+    return (seconds == 60 ? (minutes+1) + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds);
+}
+
+testNodeStatus()
