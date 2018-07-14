c400d413b70b62cef1607df3d70dc2564f29ead2
Alex Carrithers <38507729+AlexCarrithers@users.noreply.github.com>
2018-07-14 18:12:37 +0100

    Add testing for stall and offline status

---
+++ tests/failure.test.js
@@ -0,0 +1,38 @@
+const assert = require('assert');
+const axios = require('axios');
+
+describe('Check if the script handles failure logic properly', () => {
+    function Node(node) {
+        this.status = node.status;
+    }
+
+    it('should result in a stalled status and an alert call', async () => {
+        Node.prototype.monitor = function() {
+            this.lastBlockTime = Date.now() - 1800001;
+        };
+        const node = new Node({ status: 'online' });
+        await node.monitor();
+
+        if (Date.now() - node.lastBlockTime > 1800000 && node.status === 'online') {
+            node.status = 'stalled';
+            node.alertTriggered = true;
+        }
+        assert(node.status === 'stalled');
+        assert(node.alertTriggered === true);
+    });
+
+    it('should result in an offline status and an alert call', async () => {
+        Node.prototype.checkHeight = function() {
+            return null;
+        };
+        const node = new Node({ status: 'online' });
+        const nodeHeight = node.checkHeight();
+
+        if (!nodeHeight && node.status !== 'offline') {
+            node.status = 'offline';
+            node.alertTriggered = true;
+        }
+        assert(node.status === 'offline');
+        assert(node.alertTriggered === true);
+    });
+});
